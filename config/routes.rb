56a96a3adf165bfa405b680624187647c1140b2d
Kunal Agarwal <kunal94agarwal@gmail.com>
2017-10-31 04:18:57 +0530

    rendering a template

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'demo/index'
+  get 'demo/hello'
+  # get '/:controller(/:action(/:id))'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
