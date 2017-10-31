3ac054962f872186a2700760f7f66b0072090497
Kunal Agarwal <kunal94agarwal@gmail.com>
2017-10-31 14:32:53 +0530

    running a migration

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,11 +3,14 @@ class DemoController < ApplicationController
 	layout false
 
   def index
-  	render('demo/hello')
+  	render('demo/index')
   end
 
   def hello
-  	render('demo/index')
+  	@array = [1,2,3,4,5]
+  	@id = params[:id]
+  	@page = params[:page]
+  	render('demo/hello')
   end
 
   def other_hello 
