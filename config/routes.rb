4d2d5425ca0f7ddd5c08dab58a666768e6b4b9a0
Kunal Agarwal <kunal94agarwal@gmail.com>
2017-11-16 16:40:33 +0530

    generating Subjects controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'subjects/index'
+  get 'subjects/show'
+  get 'subjects/new'
+  get 'subjects/edit'
+  get 'subjects/delete'
+
   get 'demo/index'
   get 'demo/hello'
   get 'demo/other_hello'
