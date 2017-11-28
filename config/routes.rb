ebd152fbacb34c18c6567861042bc43cc1db3cbf
Kunal Agarwal <kunal94agarwal@gmail.com>
2017-11-29 05:24:18 +0530

    addind RESTful routes for sections and pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
  
-  root 'demo#index'
+  resources :sections do
+    member do
+      get :delete
+    end
+  end
+
+  resources :pages do
+    member do
+      get :delete
+    end
+  end
+
 
   resources :subjects do 
      member do 
